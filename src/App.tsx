5c5a43d22bf4b203127e1f6a93bec01197fbd3c7
Rafael Vieira <rafaelvieiras@pm.me>
2022-10-24 05:05:28 -0300

    fix: add scene and fix home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
 import styled from "@emotion/styled";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { HomePage } from "pages/HomePage";
+import { ScenePage } from "pages/ScenePage";
 
 const AppContainer = styled.div`
-  background-color: #282c34;
+  background-color: #000;
   min-height: 100vh;
   font-size: calc(10px + 2vmin);
   color: white;
+  padding: 0 20px;
 `;
 
 const App = () => {
+  const navigate = useNavigate();
   return (
     <AppContainer>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/play" element={<ScenePage navigate={navigate}/>} />
       </Routes>
     </AppContainer>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,16 @@
+import styled from "@emotion/styled";
+import { Button } from "components/button/Button";
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 export const HomePage = () => {
-  return <div>Home Page</div>;
+  return <Container>
+    <img src="squid-game-image.jpg" alt="" />
+    <h1>Jogo da Lula</h1>
+    <Button to="/play">Jogar</Button>
+  </Container>;
 };
